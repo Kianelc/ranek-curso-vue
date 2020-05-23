211ea0a45b041ef09e1e14c30155fc8899c90379
Kiane Lucia Casagrande <kiane.casagrande@hotmail.com>
2020-05-22 22:16:37 -0300

    Adicionando loading, ajuste css e modificando o titulo da pagina

--- src/views/Produto.vue
+++ src/views/Produto.vue
@@ -3,7 +3,7 @@
     <div v-if="produto" class="produto">
       <ul class="fotos" v-if="produto.fotos">
         <li v-for="(foto, index) in produto.fotos" :key="index">
-          <img :src="foto.src" :alt="foto.titulo">
+          <img :src="foto.src" :alt="foto.titulo" />
         </li>
       </ul>
       <div class="info">
@@ -12,12 +12,12 @@
         <p class="descricao">{{produto.descricao}}</p>
         <transition mode="out-in" v-if="produto.vendido === 'false'">
           <button class="btn" v-if="!finalizar" @click="finalizar = true">Comprar</button>
-          <FinalizarCompra v-else :produto="produto"/>
+          <FinalizarCompra v-else :produto="produto" />
         </transition>
-        <button v-else class="btn" disabled>Produto Vendido</button>
+        <button v-else class="btn btn-disabled" disabled>Produto Vendido</button>
       </div>
     </div>
-    <PaginaCarregando v-else/>
+    <PaginaCarregando v-else />
   </section>
 </template>
 
@@ -41,6 +41,7 @@ export default {
     getProduto() {
       api.get(`/produto/${this.id}`).then(response => {
         this.produto = response.data;
+        document.title = this.produto.nome;
       });
     }
   },
@@ -67,12 +68,39 @@ export default {
   margin-bottom: 40px;
 }
 
+.fotos {
+  grid-row: 1 / 3;
+}
+
+.info {
+  position: sticky;
+  top: 20px;
+}
+
 .descricao {
   font-size: 1.2rem;
+}
+
+img {
+  margin-bottom: 30px;
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.2);
+  border-radius: 4px;
 }
 
 .btn {
   margin-top: 60px;
   width: 200px;
+}
+
+@media screen and (max-width: 500px) {
+  .produto {
+    grid-template-columns: 1fr;
+  }
+  .fotos {
+    grid-row: 2;
+  }
+  .info {
+    position: initial;
+  }
 }
 </style>
